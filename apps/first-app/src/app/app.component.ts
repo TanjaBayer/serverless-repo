566f320814884a7b24f0db6a44cad1d45c7fd20a
Tanja Bayer <tanjabayer@Tanjas-MBP.fritz.box>
2021-01-11 19:15:46 +0100

    initial commit

---
+++ apps/first-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-serverless-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'first-app';
+}
